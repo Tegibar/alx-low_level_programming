6e13b9cbd05cc79cd4161a63cc7aea7b429e19a2
Tegibar <bgtegibar@gmail.com>
2023-03-08 01:16:25 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * wildcmp - Entry Point
- * @s1: input
- * @s2: input
+ * @s1: input one
+ * @s2: input two
  * Return: 0
  */
 int wildcmp(char *s1, char *s2)
