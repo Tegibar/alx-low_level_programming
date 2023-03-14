681d8d6696c6ca5ff14c2484b52b97dcfc0858f5
Tegibar <bgtegibar@gmail.com>
2023-03-13 17:13:58 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,40 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* alloc_grid - returns a pointer to a 2 dimensional array of integers.
-* @width: width of array
-* @height: height of array
-*
-* Return: a pointer to a 2 dimensional array of integers.
-*/
+ * alloc_grid - create a 2-dimensional array with each element set to 0
+ * @width: desired number of columns
+ * @height: desired number of rows
+ *
+ * Return: NULL if memory allocation fails or any argument is less than 1,
+ * otherwise return a pointer to the first element of the array.
+ */
 int **alloc_grid(int width, int height)
 {
-int **array, i, j;
-int len = width * height;
+int **matrix, row, column;
 
-if (len <= 0)
+if (width < 1 || height < 1)
 return (NULL);
 
-array = (int)malloc(sizeof(int *) * height);
-if (array == NULL)
+matrix = (int) malloc(sizeof(int *) * height);
+
+if (!matrix)
 return (NULL);
 
-for (i = 0; i < height; i++)
+for (row = 0; row < height; ++row)
 {
-array[i] = (int *)malloc(sizeof(int) * width);
-if (array[i] == NULL)
+matrix[row] = (int *) malloc(sizeof(int) * width);
+
+if (!matrix[row])
 {
-for (i--; i >= 0; i--)
-free(array[i]);
-free(array);
+while (--row > -1)
+free(matrix[row]);
+free(matrix);
 return (NULL);
 }
+
+for (column = 0; column < width; ++column)
+matrix[row][column] = 0;
 }
 
-for (i = 0; i < height; i++)
-for (j = 0; j < width; j++)
-array[i][j] = 0;
-
-return (array);
+return (matrix);
 }
